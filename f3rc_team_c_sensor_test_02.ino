e9dc14f8435422273a31584e3ba88395790be35c
AAKK <keiichiarimura0109@gmail.com>
2024-07-09 10:09:32 +0900

    First proto

---
+++ f3rc_team_c_sensor_test_02.ino
@@ -0,0 +1,287 @@
+#include<Wire.h>
+
+#include <VL53L0X.h>
+
+
+// BMX055 加速度センサのI2Cアドレス  
+#define Addr_Accl 0x19  // (JP1,JP2,JP3 = Openの時)
+// BMX055 ジャイロセンサのI2Cアドレス
+#define Addr_Gyro 0x69  // (JP1,JP2,JP3 = Openの時)
+// BMX055 磁気センサのI2Cアドレス
+#define Addr_Mag 0x13   // (JP1,JP2,JP3 = Openの時)
+
+
+// センサーの値を保存するグローバル変数
+float xAccl = 0.00;
+float yAccl = 0.00;
+float zAccl = 0.00;
+float xGyro = 0.00;
+float yGyro = 0.00;
+float zGyro = 0.00;
+int   xMag  = 0;
+int   yMag  = 0;
+int   zMag  = 0;
+
+
+// 使用するセンサーの数
+#define SENSOR_NUM  4 
+
+//使用するセンサーのリセットのピン
+#define SENSOR0 23
+#define SENSOR1 19
+#define SENSOR2 18
+#define SENSOR3 17
+
+#define ADDRESS_DEFAULT 0b0101001 // 0x29
+#define ADDRESS_00 (ADDRESS_DEFAULT + 2)
+/**
+ * 各センサのXSHUTへ接続されているGPIOの配列
+ */
+const int GPIO_MASK_ARRAY[SENSOR_NUM] = {SENSOR0, SENSOR1, SENSOR2, SENSOR3};
+VL53L0X gSensor[SENSOR_NUM]; // 使用するセンサークラス配列
+
+
+//何秒ごとにプログラムを動かすか(ms)
+unsigned long start_time, end_time;
+int program_period = 100;
+
+
+
+void BMX055_Init()
+{
+  //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Accl);
+  Wire.write(0x0F); // Select PMU_Range register
+  Wire.write(0x03);   // Range = +/- 2g
+  Wire.endTransmission();
+  delay(100);
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Accl);
+  Wire.write(0x10);  // Select PMU_BW register
+  Wire.write(0x08);  // Bandwidth = 7.81 Hz
+  Wire.endTransmission();
+  delay(100);
+  //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Accl);
+  Wire.write(0x11);  // Select PMU_LPW register
+  Wire.write(0x00);  // Normal mode, Sleep duration = 0.5ms
+  Wire.endTransmission();
+  delay(100);
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Gyro);
+  Wire.write(0x0F);  // Select Range register
+  Wire.write(0x04);  // Full scale = +/- 125 degree/s
+  Wire.endTransmission();
+  delay(100);
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Gyro);
+  Wire.write(0x10);  // Select Bandwidth register
+  Wire.write(0x07);  // ODR = 100 Hz
+  Wire.endTransmission();
+  delay(100);
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Gyro);
+  Wire.write(0x11);  // Select LPM1 register
+  Wire.write(0x00);  // Normal mode, Sleep duration = 2ms
+  Wire.endTransmission();
+  delay(100);
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4B);  // Select Mag register
+  Wire.write(0x83);  // Soft reset
+  Wire.endTransmission();
+  delay(100);
+  //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4B);  // Select Mag register
+  Wire.write(0x01);  // Soft reset
+  Wire.endTransmission();
+  delay(100);
+  //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4C);  // Select Mag register
+  Wire.write(0x00);  // Normal Mode, ODR = 10 Hz
+  Wire.endTransmission();
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x4E);  // Select Mag register
+  Wire.write(0x84);  // X, Y, Z-Axis enabled
+  Wire.endTransmission();
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x51);  // Select Mag register
+  Wire.write(0x04);  // No. of Repetitions for X-Y Axis = 9
+  Wire.endTransmission();
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Mag);
+  Wire.write(0x52);  // Select Mag register
+  Wire.write(16);  // No. of Repetitions for Z-Axis = 15
+  Wire.endTransmission();
+}
+//=====================================================================================//
+void BMX055_Accl()
+{
+  unsigned int data[6];
+  for (int i = 0; i < 6; i++)
+  {
+    Wire.beginTransmission(Addr_Accl);
+    Wire.write((2 + i));// Select data register
+    Wire.endTransmission();
+    Wire.requestFrom(Addr_Accl, 1);// Request 1 byte of data
+    // Read 6 bytes of data
+    // xAccl lsb, xAccl msb, yAccl lsb, yAccl msb, zAccl lsb, zAccl msb
+    if (Wire.available() == 1)
+      data[i] = Wire.read();
+  }
+  // Convert the data to 12-bits
+  xAccl = ((data[1] * 256) + (data[0] & 0xF0)) / 16;
+  if (xAccl > 2047)  xAccl -= 4096;
+  yAccl = ((data[3] * 256) + (data[2] & 0xF0)) / 16;
+  if (yAccl > 2047)  yAccl -= 4096;
+  zAccl = ((data[5] * 256) + (data[4] & 0xF0)) / 16;
+  if (zAccl > 2047)  zAccl -= 4096;
+  xAccl = xAccl * 0.00098; // range = +/-2g
+  yAccl = yAccl * 0.00098; // range = +/-2g
+  zAccl = zAccl * 0.00098; // range = +/-2g
+}
+//=====================================================================================//
+void BMX055_Gyro()
+{
+  unsigned int data[6];
+  for (int i = 0; i < 6; i++)
+  {
+    Wire.beginTransmission(Addr_Gyro);
+    Wire.write((2 + i));    // Select data register
+    Wire.endTransmission();
+    Wire.requestFrom(Addr_Gyro, 1);    // Request 1 byte of data
+    // Read 6 bytes of data
+    // xGyro lsb, xGyro msb, yGyro lsb, yGyro msb, zGyro lsb, zGyro msb
+    if (Wire.available() == 1)
+      data[i] = Wire.read();
+  }
+  // Convert the data
+  xGyro = (data[1] * 256) + data[0];
+  if (xGyro > 32767)  xGyro -= 65536;
+  yGyro = (data[3] * 256) + data[2];
+  if (yGyro > 32767)  yGyro -= 65536;
+  zGyro = (data[5] * 256) + data[4];
+  if (zGyro > 32767)  zGyro -= 65536;
+
+  xGyro = xGyro * 0.0038; //  Full scale = +/- 125 degree/s
+  yGyro = yGyro * 0.0038; //  Full scale = +/- 125 degree/s
+  zGyro = zGyro * 0.0038; //  Full scale = +/- 125 degree/s
+}
+//=====================================================================================//
+void BMX055_Mag()
+{
+  unsigned int data[8];
+  for (int i = 0; i < 8; i++)
+  {
+    Wire.beginTransmission(Addr_Mag);
+    Wire.write((0x42 + i));    // Select data register
+    Wire.endTransmission();
+    Wire.requestFrom(Addr_Mag, 1);    // Request 1 byte of data
+    // Read 6 bytes of data
+    // xMag lsb, xMag msb, yMag lsb, yMag msb, zMag lsb, zMag msb
+    if (Wire.available() == 1)
+      data[i] = Wire.read();
+  }
+// Convert the data
+  xMag = ((data[1] <<5) | (data[0]>>3));
+  if (xMag > 4095)  xMag -= 8192;
+  yMag = ((data[3] <<5) | (data[2]>>3));
+  if (yMag > 4095)  yMag -= 8192;
+  zMag = ((data[5] <<7) | (data[4]>>1));
+  if (zMag > 16383)  zMag -= 32768;
+}
+
+
+
+
+
+
+void setup()
+{
+  // Wire(Arduino-I2C)の初期化
+  Wire.begin();
+  // デバッグ用シリアル通信は115200bps
+  Serial.begin(115200);
+
+
+  //BMX055 初期化
+  BMX055_Init();
+
+
+  //VL53L0X 初期化
+  // まず全てのGPIOをLOW
+  for (int i = 0; i < SENSOR_NUM; i++)
+  {
+    pinMode(GPIO_MASK_ARRAY[i], OUTPUT);
+    digitalWrite(GPIO_MASK_ARRAY[i], LOW);
+  }
+
+  for (int i = 0; i < SENSOR_NUM; i++) {
+    // センサを初期化
+    pinMode(GPIO_MASK_ARRAY[i], INPUT);
+    if (gSensor[i].init() == true)
+    {
+      gSensor[i].setTimeout(1000);
+      gSensor[i].startContinuous(10);
+      int address = ADDRESS_00 + (i * 2);
+      gSensor[i].setAddress(address);
+    }
+    else
+    {
+      Serial.print("Sensor ");
+      Serial.print(i);
+      Serial.println(" error");
+    }
+  }
+}
+
+
+void loop()
+{
+  start_time = millis();
+  //BMX055 加速度の読み取り
+  BMX055_Accl();
+  Serial.print("Accl= ");
+  Serial.print(xAccl);
+  Serial.print(",");
+  Serial.print(yAccl);
+  Serial.print(",");
+  Serial.print(zAccl);
+  Serial.print(" "); 
+  
+  //BMX055 ジャイロの読み取り
+  BMX055_Gyro();
+  Serial.print("Gyro= ");
+  Serial.print(xGyro);
+  Serial.print(",");
+  Serial.print(yGyro);
+  Serial.print(",");
+  Serial.print(zGyro);
+  Serial.print(" "); 
+  
+  //BMX055 磁気の読み取り
+  BMX055_Mag();
+  Serial.print("Mag= ");
+  Serial.print(xMag);
+  Serial.print(",");
+  Serial.print(yMag);
+  Serial.print(",");
+  Serial.print(zMag);
+  Serial.print(" ");
+
+  //VL53L0X 距離の読み取り
+  for (int i = 0; i < SENSOR_NUM; i++) {
+    Serial.print(gSensor[i].readRangeSingleMillimeters());
+    if (gSensor[i].timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+
+    Serial.println();
+
+  
+  end_time = millis();
+  delay(program_period - (start_time - end_time));
+  }
+}
